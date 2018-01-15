c6c97fc27d6fbd135b73e63bdb3ec9d08afdb327
Gabriel Diegel <gabriel.diegel@gmail.com>
2018-01-14 23:21:18 -0500

    Solve koan

--- test/iii_conventions/multiAssignemnt/N30DestructuringDeclarationsKtTest.kt
+++ test/iii_conventions/multiAssignemnt/N30DestructuringDeclarationsKtTest.kt
@@ -5,7 +5,8 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class N30DestructuringDeclarationsKtTest {
-    @Test fun testIsLeapDay() {
+    @Test
+    fun testIsLeapDay() {
         assertTrue(isLeapDay(MyDate(2016, 2, 29)))
         assertFalse(isLeapDay(MyDate(2015, 2, 29)))
     }
