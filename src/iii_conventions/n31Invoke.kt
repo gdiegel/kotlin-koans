1a8613d1168a5c3a479932884ae3bf09bba929e5
Gabriel Diegel <gabriel.diegel@gmail.com>
2018-01-14 23:38:46 -0500

    Solve koan

--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,17 +3,23 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(var invocations: Int = 0) {
+    fun getNumberOfInvocations(): Int = invocations
+}
+
+operator fun Invokable.invoke(): Invokable {
+    invocations++
+    return Invokable(invocations)
+}
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change the class 'Invokable' to count the number of invocations:
         for 'invokable()()()()' it should be 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
