4403344f6053c2b7e6ccf550ee34fb653a267250
Gabriel Diegel <gabriel.diegel@gmail.com>
2018-01-22 19:22:08 -0500

    Solve koan

--- src/iv_properties/n34DelegatesExamples.kt
+++ src/iv_properties/n34DelegatesExamples.kt
@@ -4,13 +4,17 @@ import util.TODO
 import util.doc34
 
 class LazyPropertyUsingDelegates(val initializer: () -> Int) {
-    val lazyValue: Int by todoTask34()
+    var invoked: Int? = null
+    val lazyValue: Int by lazy {
+        invoked = initializer()
+        invoked!!
+    }
 }
 
 fun todoTask34(): Lazy<Int> = TODO(
-    """
+        """
         Task 34.
         Read about delegated properties and make the property lazy by using delegates.
     """,
-    documentation = doc34()
+        documentation = doc34()
 )
--- test/iv_properties/N34DelegatesExamplesKtTest.kt
+++ test/iv_properties/N34DelegatesExamplesKtTest.kt
@@ -4,7 +4,8 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class N34DelegatesExamplesKtTest {
-    @Test fun testLazy() {
+    @Test
+    fun testLazy() {
         var initialized = false
         val lazyProperty = LazyPropertyUsingDelegates({ initialized = true; 42 })
         assertFalse("Property shouldn't be initialized before access", initialized)
@@ -13,9 +14,10 @@ class N34DelegatesExamplesKtTest {
         assertEquals(42, result)
     }
 
-    @Test fun initializedOnce() {
+    @Test
+    fun initializedOnce() {
         var initialized = 0
-        val lazyProperty = LazyPropertyUsingDelegates( { initialized++; 42 })
+        val lazyProperty = LazyPropertyUsingDelegates({ initialized++; 42 })
         lazyProperty.lazyValue
         lazyProperty.lazyValue
         assertEquals("Lazy property should be initialized once", 1, initialized)
