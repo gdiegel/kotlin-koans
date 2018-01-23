ea0b699c71876fc57d5a1e388e9cde50dbeb6097
Gabriel Diegel <gabriel.diegel@gmail.com>
2018-01-23 13:28:53 -0500

    Solve koan

--- test/v_builders/N36ExtensionFunctionLiteralsKtTest.kt
+++ test/v_builders/N36ExtensionFunctionLiteralsKtTest.kt
@@ -4,7 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class N36ExtensionFunctionLiteralsKtTest {
-    @Test fun testIsOddAndIsEven() {
+    @Test
+    fun testIsOddAndIsEven() {
         val result = task36()
         assertEquals("The functions 'isOdd' and 'isEven' should be implemented correctly",
                 listOf(false, true, true), result)
